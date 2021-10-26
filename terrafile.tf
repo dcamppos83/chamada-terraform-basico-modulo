d60eb83d90a385817fd1cdf7b04ba5df75c9faae
Daniel Campos <danielcampos@Supportes-MacBook-Pro.local>
2021-10-26 10:52:41 -0300

    Initial commit

--- terrafile.tf
+++ terrafile.tf
@@ -13,7 +13,8 @@ terraform {
 }
 
 module "produto" {
-  source  =  "git@github.com:dcamppos83/terraform-basico-modulo.git?ref=v1.0.0"
+  #source  =  "git@github.com:dcamppos83/terraform-basico-modulo.git?ref=v1.0.0"
+  source  =  "git@github.com:dcamppos83/terraform-basico-modulo.git?ref=main"
   environment = "production"
 }
 
